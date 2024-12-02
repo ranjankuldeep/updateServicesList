18905748298e44859e8b8a1d293385679125983f
kuldeep ranjan <ranjankuldeep19@gmail.com>
2024-12-02 21:12:02 +0530

    feat: fixed main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ranjankuldeep/updateServicesList/internal/database"
+	"github.com/ranjankuldeep/updateServicesList/internal/runner"
+)
+
+func main() {
+	// Load environment variables
+	username := os.Getenv("MONGODB_USERNAME")
+	password := os.Getenv("MONGODB_PASSWORD")
+	databaseName := os.Getenv("MONGODB_DATABASE")
+	uri := fmt.Sprintf("mongodb+srv://%s:%s@cluster0.blfflhg.mongodb.net/%s?retryWrites=true&w=majority", username, password, databaseName)
+
+	client, err := database.ConnectDB(uri)
+	if err != nil {
+		log.Fatal("Error initializing MongoDB connection:", err)
+	}
+	db := client.Database(databaseName)
+	startTime := time.Now()
+	runner.UpdateServerData(db, context.TODO())
+
+	duration := time.Since(startTime).Seconds()
+	durationRoundOff := fmt.Sprintf("%.2f seconds", duration)
+	fmt.Println("Time taken to update server data:", durationRoundOff)
+}
